e4cce88de74b178b1fdade220561d3c2eae85426
Benoit LeBlanc <benoitleblanc@Benoits-MacBook-Pro.local>
2021-05-31 10:18:47 -0300

    Changed jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  compilerOptions: {
+    module: "commonjs",
+    target: "esnext",
+    jsx: "react",
+    sourceMap: true,
+    experimentalDecorators: true,
+    noImplicitUseStrict: true,
+    removeComments: true,
+    moduleResolution: "node",
+    lib: ["es2017", "dom"],
+    typeRoots: ["node_modules/@types", "src/@types"],
+  },
+  exclude: ["node_modules", "out", ".next"],
 };
